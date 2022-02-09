3792f0dc6dd8c6cdde0b04b0afca3da9ae58e6e7
Karyme Nava <s19120124@alumnos.itsur.edu.mx>
2022-02-09 15:22:33 -0600

    Agregué el código de los POJOS

--- ProyectoBD/POJOS/clsCategorias.cs
+++ ProyectoBD/POJOS/clsCategorias.cs
@@ -8,5 +8,44 @@ namespace ProyectoBD.POJOS
 {
     class clsCategorias
     {
+        private int idCategoria;
+        private string nombreCategoria;
+        private string descripcion;
+
+        public int IDCategoria
+        {
+            get
+            {
+                return idCategoria;
+            }
+            set
+            {
+                idCategoria = value;
+            }
+        }
+
+        public string NombreCategoria
+        {
+            get
+            {
+                return nombreCategoria;
+            }
+            set
+            {
+                nombreCategoria = value;
+            }
+        }
+
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+            set
+            {
+                descripcion = value;
+            }
+        }
     }
 }
